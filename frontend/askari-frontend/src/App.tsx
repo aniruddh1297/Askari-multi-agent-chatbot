24705b32955a60fd3e226129d39b57a24355e34c
Ani <aniruddh1297@gmail.com>
2025-06-28 18:52:29 +0200

    minor bug fixes

--- frontend/askari-frontend/src/App.tsx
+++ frontend/askari-frontend/src/App.tsx
@@ -111,7 +111,7 @@ const App: React.FC = () => {
                       {children}
                     </code>
                   ) : (
-                    <>{children}</> /
+                    <>{children}</> 
                   ),
               }}
             >
